7f80c63fcc91ea60f418d3bde1f0af3c32bea202
Uma821 <uma.ohmoto@gmail.com>
2023-01-20 22:59:24 +0900

    指定したサイトのスクレイピングが行えるようになった

--- srcs/scraping_kuruken.py
+++ srcs/scraping_kuruken.py
@@ -8,10 +8,17 @@ from bs4 import BeautifulSoup
 
 
 def find_bus_data(page_source):
-  daiya_list = BeautifulSoup(page_source, "html.perser").find(id="approach_list_for_daiya")
-  for daiya in daiya_list.find_all("li"):
-    daiya
-  return [()] # (後なん分，時間，目標時間) 
+  daiya_list = BeautifulSoup(page_source, "html.parser").find(id="approach_list_for_daiya")
+  bus_datas = []
+  for daiya in daiya_list.find_all("li", limit=3):
+    (time, course_name, delay_minutes) = daiya.tr("td") # 1列目には到着時刻，目的(経由)地，残り時間が示される find_all
+    bus_datas.append((
+      time.text.strip(), 
+      course_name.a.text, 
+      dict(enumerate(getattr(course_name.div, "stripped_strings", [None]))).get(1), # course_name.div.stringが存在しないならNone (経由地が存在しない)
+      '1' if "soon" in delay_minutes["class"] else dict(enumerate(delay_minutes.div.stripped_strings)).get(1), # 存在しないときにNoneにさせたい
+    )) # (到着時刻，目的地，経由地，目標時間) 
+  return bus_datas
 
 def scraping_kuruken(sites):
   options = Options()
@@ -26,7 +33,7 @@ def scraping_kuruken(sites):
     service = Service(ChromeDriverManager().install())
 
   driver = webdriver.Chrome(options=options, service=service)
-  return [[driver.get(site), time.sleep(20), driver.page_source][2] for site in sites]
+  return [find_bus_data([driver.get(site), time.sleep(20), driver.page_source][2]) for site in sites]
 
 if __name__ == "__main__": # テストするならこのif文の中で
   print(scraping_kuruken(["https://kuruken.jp/Approach?sid=8cdf9206-6a32-4ba9-8d8c-5dfdc07219ca&noribaChange=1"]))
